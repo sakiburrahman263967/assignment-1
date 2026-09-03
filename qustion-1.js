02c4c2d0e5deb4096ecffee234f9ec7a4a4fb5f3
sakiburrahman263967 <sakiburahman782@gmail.com>
2026-09-03 12:32:30 +0600

    first commit

---
+++ qustion-1.js
@@ -0,0 +1,18 @@
+// Question 1 value detect korte hobe. typeof and truth/ falsy concept vevohar korte hobe 
+function describeValue(value){
+     let type = typeof value;
+     let result;
+
+
+     if (value) {
+        result = "truthy"
+     } else {
+        result= "falsy"
+     }
+
+     return type + "  | " + result ;
+
+     
+}
+console.log(describeValue ("helo"));
+
