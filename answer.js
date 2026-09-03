8ae67bd450a96bf9b81150e706265e4ff364546a
sakiburrahman263967 <sakiburahman782@gmail.com>
2026-09-03 12:39:59 +0600

    answer1 commit

--- answer.js
+++ answer.js
@@ -15,14 +15,3 @@ function describeValue(value){
      
 }
 
-
-console.log(describeValue ("helo"));
-console.log(describeValue(""));
-console.log(describeValue(25));
-console.log(describeValue(0));
-console.log(describeValue(true));
-console.log(describeValue(null));
-console.log(describeValue(undefined));
-
-console.log(describeValue("0"));
-console.log(describeValue(NaN));
