b4e526c639b8b9c481b20cfa856edda3d78b4842
sakiburrahman263967 <sakiburahman782@gmail.com>
2026-09-03 15:14:45 +0600

    qusiton5 commit

---
+++ qustion5.js
@@ -0,0 +1,50 @@
+const getChaseVerdict = (target, scored, ballsLeft) => {
+
+    let runsNeeded = target - scored;
+
+
+
+
+
+
+    if (runsNeeded <= 0) {
+        return "Won";
+    }
+
+
+
+
+
+    if (ballsLeft <= 0) {
+        return "Lost";
+    }
+
+
+    let requiredRate = (runsNeeded / ballsLeft) * 6;
+
+    let verdict;
+
+    if (requiredRate <= 6) {
+        verdict = "Comfortable";
+    } else if (requiredRate <= 12) {
+        verdict = "Tough";
+    } else {
+        verdict = "Almost Impossible";
+    }
+
+
+
+
+
+
+    
+    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
+
+
+console.log(getChaseVerdict(200, 200, 12));
+console.log(getChaseVerdict(200, 190, 0));
+console.log(getChaseVerdict(100, 90, 12));
+console.log(getChaseVerdict(100, 80, 12));
+console.log(getChaseVerdict(100, 70, 12));
+console.log(getChaseVerdict(150, 149, 1));
