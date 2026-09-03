542f6b36907929f7aa45b553538c557cbefa81ce
sakiburrahman263967 <sakiburahman782@gmail.com>
2026-09-03 13:50:23 +0600

    ans3  final  committ

--- answer.js
+++ answer.js
@@ -43,3 +43,25 @@ function getDayType(day){
 
 
 
+
+
+// okay qustion3 bola hoyecche ekta validUserName function banate hobe jeta user name niye nirdrishto niyom golo cheak korbe she ono jayi Too Short No Space Allowed Reserved Word or Available return korbe
+function  validateUsername (username){
+
+    if(username.lenght < 4 ){
+        return "Tooooo short";
+    }
+
+     if (username.includes(" ")) {
+        return "No Space Allowed";
+    }
+
+      if (username.toLowerCase().includes("admin")) {
+        return "Reserved Word";
+    }
+
+    return "Available";
+    
+   
+}
+
