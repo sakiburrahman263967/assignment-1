a43b1e4ebba776375dbeccc23d11f5189cf14fbd
sakiburrahman263967 <sakiburahman782@gmail.com>
2026-09-03 13:25:04 +0600

    anser2 final committ

--- answer.js
+++ answer.js
@@ -15,3 +15,31 @@ function describeValue(value){
      
 }
 
+
+// okay qusion 2 amader bola hoyeche jekono ekta day er name dile sheta working day naki wekand day or invalid day like moons day return korte hobe 
+function getDayType(day){
+    day = day.toLowerCase();
+
+
+
+    switch (day){
+        case "friday":
+        case "saturday":
+
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+}
+
+
+
+
